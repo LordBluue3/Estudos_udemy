97f7a90a0b39a8678503e5d2d13347c13b5b3cda
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-03 21:34:46 -0300

    Modulo de arrays da alura

---
+++ src/main/java/alura/modulo2/Arrays.java
@@ -0,0 +1,12 @@
+package alura.modulo2;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int [] ar = new int[10];
+
+        ar[1] = 101;
+        System.out.println(ar[1]);
+
+    }
+}
