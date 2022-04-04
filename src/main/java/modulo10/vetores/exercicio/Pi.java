d9ebf2ee4cbb72a8e0ebf5bcf1f2154d724b2249
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-03 21:40:57 -0300

    Realizando calculos para saber o valor de pi

---
+++ src/main/java/modulo10/vetores/exercicio/Pi.java
@@ -0,0 +1,11 @@
+package modulo10.vetores.exercicio;
+
+public class Pi {
+    public static void main(String[] args) {
+        double d = 9.4;
+        double c = 29.6;
+
+        double pi = c/d;
+        System.out.println(pi);
+    }
+}
