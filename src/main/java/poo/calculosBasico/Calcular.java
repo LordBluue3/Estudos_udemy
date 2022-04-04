100227f0ad6102e3da955f0289765a31a1803b37
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-03 21:36:05 -0300

    Realiza calculos básicos, soma, subtração, mutiplicação e adição

---
+++ src/main/java/poo/calculosBasico/Calcular.java
@@ -0,0 +1,17 @@
+package poo.calculosBasico;
+
+public class Calcular {
+
+    public double valor1, valor2;
+
+
+
+    public double soma (){return valor1 + valor2;}
+
+    public double subtracao (){return valor1 - valor2;}
+
+    public double multiplicacao (){return valor1 * valor2;}
+
+    public double divisao (){return valor1 / valor2;}
+
+}
