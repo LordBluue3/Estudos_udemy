b1f5935237d3ed524546b14a8f1e04a3ae4f2393
LordBluue3 <mikaelcandido71@gmail.com>
2022-04-03 21:38:05 -0300

    Apresentação

---
+++ src/main/java/programa/Mikael.java
@@ -0,0 +1,9 @@
+package programa;
+
+public class Mikael {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo, me chamo Mikael Oliveira");
+        System.out.println("Sou formado em Análise e Desenvolvimento de Sistemas");
+        System.out.print("Focado em aprender coisas novas!! :)");
+    }
+}
